925cfb3366e7bbb995162910f0d8ce16245a9a33
cviejo <carlos.viejo.muros@gmail.com>
2022-11-20 13:55:33 +0100

    added translate plugin

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -18,9 +18,8 @@ require('packer').startup(function(use)
 	use 'meain/vim-printer'
 	use 'mizlan/iswap.nvim'
 	use 'numToStr/Comment.nvim'
+	use 'uga-rosa/translate.nvim'
 
-	use {'Pocco81/true-zen.nvim', requires = {devicons}}
-	use {'romgrk/barbar.nvim', requires = {devicons}}
 	use {'ziglang/zig.vim', disable = vscode}
 	use {'jbyuki/venn.nvim', disable = vscode}
 	use {'windwp/nvim-autopairs', disable = vscode}
@@ -33,16 +32,21 @@ require('packer').startup(function(use)
 	use {'tpope/vim-fugitive', disable = vscode}
 	use {'HiPhish/awk-ward.nvim', disable = vscode}
 	use {'NicholasDunham/chuck.nvim', disable = vscode}
-	use {"catppuccin/nvim", as = "catppuccin", disable = vscode}
-	use {'neoclide/coc.nvim', branch = 'release', disable = vscode}
-	use {'xolox/vim-notes', requires = {'xolox/vim-misc'}, disable = vscode}
-	use {'nvim-lualine/lualine.nvim', requires = {devicons, opt = true}, disable = vscode}
-	use {'styled-components/vim-styled-components', branch = 'main', disable = vscode}
+	use {'Pocco81/true-zen.nvim', disable = vscode, requires = {devicons}}
+	use {'romgrk/barbar.nvim', disable = vscode, requires = {devicons}}
+	use {"catppuccin/nvim", disable = vscode, tag = 'v0.2.2', as = "catppuccin"}
+	use {'neoclide/coc.nvim', disable = vscode, branch = 'release'}
+	use {'xolox/vim-notes', disable = vscode, requires = {'xolox/vim-misc'}}
+	use {'nvim-lualine/lualine.nvim', disable = vscode, requires = {devicons, opt = true}}
+	use {'styled-components/vim-styled-components', disable = vscode, branch = 'main'}
+
+	-- evaluating:
+	use 'RRethy/vim-illuminate'
 
 	-- not ready for use yet:
-	-- use 'Pocco81/true-zen.nvim'
-	-- use 'kylechui/nvim-surround'
-	-- use {'phaazon/mind.nvim', requires = {'nvim-lua/plenary.nvim', tag = 'v1.*'}}
+	-- 'kylechui/nvim-surround'
+	-- 'rcarriga/nvim-notify'
+	-- 'phaazon/mind.nvim', -- this has a lot of potential, but no docs whatsoever
 end)
 
 -- LuaFormatter off
@@ -65,13 +69,13 @@ F.assign(vim.g, {
 		'https://github.com/nathanchapman/vscode-javascript-snippets'
 	},
 	coc_snippet_next = '<tab>',
-	fzf_layout = {
-		window = { width = 1, height = 0.4, yoffset = 0.99, border ='top' }
-	},
 	fzf_colors = {
 		border = { 'fg', 'VertSplit' }
 	},
-	-- fzf_layout = { down = '50%' },
+	fzf_layout = { down = '40%' },
+	-- fzf_layout = {
+	-- 	window = { width = 1, height = 0.4, yoffset = 0.99, border ='top' }
+	-- },
 	notes_word_boundaries = 1,
 	slime_default_config = {
 		target_pane = '{next}',
@@ -104,6 +108,10 @@ require('hop').setup({keys = 'asdfjkl;weiocmr'})
 require('iswap').setup({flash_style = 'none', autoswap = true, hl_snipe = 'ErrorMsg'})
 
 require('Comment').setup({mappings = false})
+
+vim.g.deepl_api_auth_key = os.getenv("DEEPL_KEY")
+
+require("translate").setup({default = {command = "deepl_free", output = 'replace'}})
 
 if vscode then
 	return
