6f54241e33f0d814ad695244664fc56fe86c4b50
Carlos Viejo <viejo@Carloss-MBP.sphairon.box>
2023-12-21 13:42:35 +0100

    slime chuck formatting

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -20,32 +20,32 @@ require('packer').startup(function(use)
 	use 'numToStr/Comment.nvim'
 	use 'uga-rosa/translate.nvim'
 
-	use {'stevearc/oil.nvim', disable = vscode}
-	use {'github/copilot.vim', disable = vscode}
-	use {'ziglang/zig.vim', disable = vscode}
-	use {'jbyuki/venn.nvim', disable = vscode}
-	use {'windwp/nvim-autopairs', disable = vscode}
-	use {'jpalardy/vim-slime', disable = vscode}
-	use {'junegunn/fzf', disable = vscode}
-	use {'junegunn/fzf.vim', disable = vscode}
-	use {'norcalli/nvim-colorizer.lua', disable = vscode}
-	use {'sheerun/vim-polyglot', disable = vscode}
-	use {'tpope/vim-fugitive', disable = vscode}
-	use {'HiPhish/awk-ward.nvim', disable = vscode}
-	use {'NicholasDunham/chuck.nvim', disable = vscode}
-	use {'Pocco81/true-zen.nvim', disable = vscode, requires = {devicons}}
-	use {'romgrk/barbar.nvim', disable = vscode, requires = {devicons}}
-	use {"catppuccin/nvim", disable = vscode, as = "catppuccin"}
-	use {'neoclide/coc.nvim', disable = vscode, branch = 'release'}
-	use {'xolox/vim-notes', disable = vscode, requires = {'xolox/vim-misc'}}
-	use {'styled-components/vim-styled-components', disable = vscode, branch = 'main'}
+	use { 'stevearc/oil.nvim', disable = vscode }
+	use { 'github/copilot.vim', disable = vscode }
+	use { 'ziglang/zig.vim', disable = vscode }
+	use { 'jbyuki/venn.nvim', disable = vscode }
+	use { 'windwp/nvim-autopairs', disable = vscode }
+	use { 'jpalardy/vim-slime', disable = vscode }
+	use { 'junegunn/fzf', disable = vscode }
+	use { 'junegunn/fzf.vim', disable = vscode }
+	use { 'norcalli/nvim-colorizer.lua', disable = vscode }
+	use { 'sheerun/vim-polyglot', disable = vscode }
+	use { 'tpope/vim-fugitive', disable = vscode }
+	use { 'HiPhish/awk-ward.nvim', disable = vscode }
+	use { 'NicholasDunham/chuck.nvim', disable = vscode }
+	use { 'Pocco81/true-zen.nvim', disable = vscode, requires = { devicons } }
+	use { 'romgrk/barbar.nvim', disable = vscode, requires = { devicons } }
+	use { "catppuccin/nvim", disable = vscode, as = "catppuccin" }
+	use { 'neoclide/coc.nvim', disable = vscode, branch = 'release' }
+	use { 'xolox/vim-notes', disable = vscode, requires = { 'xolox/vim-misc' } }
+	use { 'styled-components/vim-styled-components', disable = vscode, branch = 'main' }
 
 	-- evaluating:
 	use 'RRethy/vim-illuminate'
-	use({"Bryley/neoai.nvim", require = {"MunifTanjim/nui.nvim"}})
+	use({ "Bryley/neoai.nvim", require = { "MunifTanjim/nui.nvim" } })
 	use({
 		"jackMort/ChatGPT.nvim",
-		requires = {"MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim"}
+		requires = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" }
 	})
 
 	-- not ready for use yet:
@@ -97,9 +97,9 @@ F.assign(vim.g, {
 -- LuaFormatter on
 
 require('nvim-treesitter.configs').setup({
-	ensure_installed = {'c', 'lua', 'javascript', 'cpp', 'query', 'json', 'typescript'},
+	ensure_installed = { 'c', 'lua', 'javascript', 'cpp', 'query', 'json', 'typescript' },
 	sync_install = false,
-	highlight = {enable = true},
+	highlight = { enable = true },
 	disable = function(lang)
 		return lang == 'svelte'
 	end,
@@ -116,13 +116,13 @@ require('nvim-treesitter.configs').setup({
 	}
 })
 
-require('hop').setup({keys = 'asdfjkl;weiocmr'})
+require('hop').setup({ keys = 'asdfjkl;weiocmr' })
 
-require('iswap').setup({flash_style = 'none', autoswap = true, hl_snipe = 'ErrorMsg'})
+require('iswap').setup({ flash_style = 'none', autoswap = true, hl_snipe = 'ErrorMsg' })
 
-require('Comment').setup({mappings = false})
+require('Comment').setup({ mappings = false })
 
-require("translate").setup({default = {command = "deepl_free", output = 'replace'}})
+require("translate").setup({ default = { command = "deepl_free", output = 'replace' } })
 
 if vscode then
 	return
@@ -133,18 +133,27 @@ _G.slimeEscapeJavascript = function(x)
 	return x:gsub('const[%s]+', 'var '):gsub('let[%s]+', 'var '):gsub('[%s]+%.', '.')
 end
 
+-- for chuck, remove newlines
+_G.slimeEscapeChuck = function(x)
+	return x:gsub('[\n\r]+[%s]+', '\n'):gsub('[\n\r]+', ' ') .. '\n'
+end
+
 vim.cmd([[
 function SlimeOverride_EscapeText_javascript(text)
 	return v:lua.slimeEscapeJavascript(a:text)
 endfunction
+
+function SlimeOverride_EscapeText_chuck(text)
+	return v:lua.slimeEscapeChuck(a:text)
+endfunction
 ]])
 
-vim.api.nvim_create_autocmd({"FileType"}, {
-	pattern = {'fzf'},
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { 'fzf' },
 	callback = function()
-		F.assign(vim.opt, {showmode = false, ruler = false})
+		F.assign(vim.opt, { showmode = false, ruler = false })
 		onBufLeave(function()
-			F.assign(vim.opt, {showmode = true, ruler = true})
+			F.assign(vim.opt, { showmode = true, ruler = true })
 		end)
 	end
 })
@@ -153,7 +162,7 @@ vim.cmd('colorscheme catppuccin')
 
 require("oil").setup({
 	default_file_explorer = true,
-	view_options = {show_hidden = true},
+	view_options = { show_hidden = true },
 	delete_to_trash = true
 })
 
@@ -167,9 +176,9 @@ require('true-zen').setup({
 				top = 5
 			}
 		},
-		narrow = {folds_style = 'invisible'}
+		narrow = { folds_style = 'invisible' }
 	},
-	integrations = {tmux = true}
+	integrations = { tmux = true }
 })
 
 require('bufferline').setup({
@@ -179,19 +188,20 @@ require('bufferline').setup({
 	maximum_padding = 1,
 	icons = {
 		button = '',
-		modified = {buton = ''},
-		inactive = {left = '', right = '', button = ''},
-		separator = {left = '', right = ''}
+		modified = { buton = '' },
+		inactive = { left = '', right = '', button = '' },
+		separator = { left = '', right = '' }
 	},
 	add_in_buffer_number_order = false
 })
 
-require("catppuccin").setup({integrations = {treesitter = true, bufferline = true}})
+require("catppuccin").setup({ integrations = { treesitter = true, bufferline = true } })
 
-require('nvim-autopairs').setup({disable_in_macro = true})
+require('nvim-autopairs').setup({ disable_in_macro = true })
 
 require('utils.venn')
 
 -- evaluating
 require("neoai").setup()
+
 require("chatgpt").setup()
